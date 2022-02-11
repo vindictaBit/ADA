ce22c37be7f6f584169f268e1a010773bdc01354
vindictaBit <83889542+vindictaBit@users.noreply.github.com>
2022-02-10 22:40:58 -0500

    Update Bipartito.java

--- lab4/Bipartito.java
+++ lab4/Bipartito.java
@@ -5,20 +5,19 @@ class BiGraf {
     public boolean Comprobar(int[][] grafo) {
         int n = grafo.length;
         int[] color = new int[n];
-        Arrays.fill(color,-1);				// Podría ser 0 == azul / 1 == rojo / -1 == sin color
+        Arrays.fill(color,-1);				// PodrÃ­a ser 0 == azul / 1 == rojo / -1 == sin color
 
         for(int inicio = 0; inicio < n; inicio++){
-            Stack<Integer> pila = new Stack<>();  // Usamos la pila para el recorrido del grafo
+            Stack<Integer> pila = new Stack<>();  // Usaremos la pila para el recorrido del grafo
             pila.push(inicio);
             color[inicio] = 0; 					
             
             while (!pila.isEmpty()){
                 Integer vertice = pila.pop();
                 if (color[vertice] == -1){
-                	// Si el color del nodo actual es el mismo que el del nodo adyacente a él, 
-                	// significa que no es posible colorear y el grafo no es un grafo bipartito.
-                	
-                	// Para esto recorremos el grafo
+                	// Si el color del nodo actual es el mismo que el del nodo adyacente a Ã©l, 
+                	// significa que no es posible colorear y el grafo de entrada no es un grafo bipartito.
+                	// Para comprobar lo anterior, recorremos el grafo
                     for (int adj : grafo[vertice]){ 
                         if (color[adj] == -1){
                             pila.push(adj);
@@ -38,6 +37,6 @@ public class Bipartito {
 	public static void main(String[] args) {
 		BiGraf grafo = new BiGraf();
 		int[][] graf = {{1,3}, {0,2}, {1,3}, {0,2}};
-		System.out.println("¿El grafo es bipartito? " + grafo.Comprobar(graf));
+		System.out.println("Â¿El grafo es bipartito? " + grafo.Comprobar(graf));
 	}
 }
